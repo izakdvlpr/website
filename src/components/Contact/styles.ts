693c56b3fa29b82688ad48e3ada6c1025be26391
zevdvlpr <zevdvlpr@gmail.com>
2020-09-20 14:19:36 -0300

    fix(index.tsx): add background color

--- src/components/Contact/styles.ts
+++ src/components/Contact/styles.ts
@@ -1,7 +1,8 @@
+import { shade } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 100vh;
 
-  background: pink;
+  background-color: ${({ theme }) => shade(0.4, theme.secondary)};
 `;
