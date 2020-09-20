3318fe4a1f0238dba7c32df25a6f1ef3735e21ee
zevdvlpr <zevdvlpr@gmail.com>
2020-09-20 12:10:17 -0300

    chore(index.tsx): home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
-import Home from './Home';
+import Head from 'next/head';
+import React from 'react';
 
-export default Home;
+import About from '@components/About';
+import Contact from '@components/Contact';
+import Presentation from '@components/Presentation';
+import Projects from '@components/Projects';
+import Skills from '@components/Skills';
+import { Container } from '@styles/pages/index';
+
+const HomePage = () => (
+  <>
+    <Head>
+      <title>Zev | Portfólio</title>
+    </Head>
+
+    <Container>
+      <Presentation />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </Container>
+  </>
+);
+
+export default HomePage;
