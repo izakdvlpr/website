049f3bd0b13c25bb634ddcb9230581386296ade7
zevdvlpr <zevdvlpr@gmail.com>
2020-09-20 20:24:55 -0300

    chore: projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
 
-import { Container, Category } from './styles';
+import { projects } from '@data';
+
+import {
+  Container,
+  Category,
+  Column,
+  Card,
+  CardImage,
+  CardContent,
+  CardFooter,
+} from './styles';
 
 const Projects: React.FC = () => {
   return (
-    <Container>
+    <Container id="projects">
       <Category>
         {'<'}Projetos{' />'}
       </Category>
+
+      <Column>
+        {projects.map(project => (
+          <Card key={project.name}>
+            <CardImage src={project.image} alt="card image" />
+
+            <CardContent>
+              <a
+                href={project.github_page}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h1>{project.name}</h1>
+              </a>
+              <p>{project.description}</p>
+            </CardContent>
+
+            <CardFooter>
+              {project.tags.map(tag => (
+                <span key={tag}>{tag}</span>
+              ))}
+            </CardFooter>
+          </Card>
+        ))}
+      </Column>
     </Container>
   );
 };
--- src/components/Projects/styles.ts
+++ src/components/Projects/styles.ts
@@ -1,8 +1,7 @@
-import { shade } from 'polished';
+import { shade, lighten } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100vh;
   padding: 40px 80px;
 
   background-color: ${({ theme }) => shade(0.2, theme.secondary)};
@@ -20,5 +19,91 @@ export const Category = styled.h4`
 
   @media (max-width: 768px) {
     font-size: 0.9rem;
+  }
+`;
+
+export const Column = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const Card = styled.li`
+  width: 45%;
+  height: 45%;
+
+  margin: 15px;
+
+  border-radius: 6px;
+
+  border: 3px solid ${({ theme }) => shade(0.4, theme.primary)};
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const CardImage = styled.img`
+  width: 100%;
+  height: 100%;
+
+  flex-shrink: 0;
+
+  border-radius: 3px 3px 0 0;
+`;
+
+export const CardContent = styled.div`
+  padding: 15px 30px;
+
+  display: grid;
+
+  grid-template-columns: 1fr;
+  grid-auto-rows: minmax(min-content, max-content);
+
+  text-align: center !important;
+
+  > a h1 {
+    font-size: 1.8rem;
+
+    transition: color 0.2s ease-in-out;
+
+    &:hover {
+      color: ${({ theme }) => shade(0.4, theme.primary)};
+    }
+
+    @media (max-width: 768px) {
+      font-size: 1.5rem;
+    }
+  }
+
+  > p {
+    font-size: 0.9rem;
+
+    @media (max-width: 768px) {
+      font-size: 0.7rem;
+    }
+  }
+`;
+
+export const CardFooter = styled.div`
+  padding: 15px 30px;
+
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+  > span {
+    margin: 2px;
+    padding: 5px;
+
+    border-radius: 4px;
+
+    background-color: ${({ theme }) => shade(0.4, theme.secondary)};
+
+    @media (max-width: 768px) {
+      font-size: 0.8rem;
+    }
   }
 `;
