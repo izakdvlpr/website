3b24540ee1957d9a8dbbcafe6fd0993cb704ddbf
zevdvlpr <zevdvlpr@gmail.com>
2020-09-20 20:26:29 -0300

    chore: data

---
+++ src/data/index.ts
@@ -0,0 +1,27 @@
+const projects = [
+  {
+    name: 'Orangutan',
+    description:
+      'Orangutan é um bot feito para o aplicativo de bate-papo Discord. Sua função é gerenciar e proteger sua comunidade do Discord deixando à mais alegre de pessoas espaçosas.',
+    image: '/static/images/orangutan-screenshot.jpg',
+    github_page: 'https://github.com/zevdvlpr/orangutan',
+    tags: ['NodeJS', 'ExpressJS', 'TypeScript', 'API', 'DiscordJS'],
+  },
+  {
+    name: 'Drocsid',
+    description: 'Drocsid é um chat virtual, para conversar com amigos.',
+    image: '/static/images/drocsid-screenshot.jpg',
+    github_page: 'https://github.com/zevdvlpr/drocsid',
+    tags: ['ReactJS', 'NodeJS', 'ExpressJS', 'TypeScript', 'API'],
+  },
+  {
+    name: 'zevdvlpr',
+    description:
+      'Meu site (sim, este) é onde eu mostro minhas habilidades, trabalho e qualquer outra coisa que eu decida colocar aqui.',
+    image: '/static/images/zevdvlpr-screenshot.jpg',
+    github_page: 'https://github.com/zevdvlpr/zevdvlpr.ml',
+    tags: ['ReactJS', 'NextJS', 'TypeScript', 'Styled Components'],
+  },
+];
+
+export { projects };
