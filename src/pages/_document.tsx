7a7efd76d4253036a8196bbf43a81054e9c0fdc7
zevdvlpr <zevdvlpr@gmail.com>
2020-09-20 12:09:32 -0300

    chore(_document.tsx): additional title and description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,54 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+
+          <link itemProp="url" href="https://zevdvlpr.ml" />
+          <meta itemProp="name" content="Zev | Portfólio" />
+          <meta
+            itemProp="description"
+            content="Meu portfólio nele eu conto mais sobre mim 😄"
+          />
+
+          <meta name="image" content="https://github.com/zevdvlpr.png" />
+          <meta
+            name="description"
+            content="Meu portfólio nele eu conto mais sobre mim 😄"
+          />
+
+          <meta property="og:title" content="Zev | Portfólio" />
+          <meta property="og:url" content="https://zevdvlpr.ml" />
+          <meta
+            property="og:description"
+            content="Meu portfólio nele eu conto mais sobre mim 😄"
+          />
+
+          <meta property="og:image:width" content="1920" />
+          <meta property="og:image:height" content="1080" />
+          <meta property="og:image" content="https://github.com/zevdvlpr.png" />
+          <meta
+            property="og:image:secure_url"
+            content="https://github.com/zevdvlpr.png"
+          />
+
+          <link rel="icon" href="/icon.gif" />
+          <link rel="shortcut icon" href="/icon.gif" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
