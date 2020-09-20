76586944426e8589af41f6f4a59ff19911807b37
zevdvlpr <zevdvlpr@gmail.com>
2020-09-20 14:20:29 -0300

    fix(index.tsx): list

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,9 +1,86 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Category, List, Item, ExternalLink } from './styles';
 
 const Skills: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Category>
+        {'<'}Habilidades{' />'}
+      </Category>
+
+      <List>
+        <Item color="#EFD81D">
+          <h1>JavaScript</h1>
+          <p>
+            Aprendi javascript como minha primeira linguagem de programação com
+            a{' '}
+            <ExternalLink href="https://youtube.com/rocketseat" color="#EFD81D">
+              Rocketseat
+            </ExternalLink>
+            . Gosto muito de trabalhar com Javascript no Front-end e no
+            Back-end, especificamente no Node.JS. JS é tão legal que você pode
+            criar app para web até apps mobile com apenas uma linguagem.
+          </p>
+        </Item>
+        <Item color="#DD4B25">
+          <h1>HTML/CSS</h1>
+          <p>
+            HTML e CSS são essenciais para o front-end. De que outra forma você
+            espera criar um site? Enquanto todos nós odiamos CSS, ainda é o
+            padrão e com boas razões. Ah, e HTML não é uma linguagem :c
+          </p>
+        </Item>
+        <Item color="#37D1F7">
+          <h1>React</h1>
+          <p>
+            React JS e React Native são absolutamente incríveis. É a única coisa
+            boa que o Facebook trouxe para este mundo. Se eu for criar um site,
+            provavelmente o construirei com o React JS, porque o HTML simples
+            para a web é doloroso demais.
+          </p>
+        </Item>
+        <Item color="#61AE51">
+          <h1>MongoDB</h1>
+          <p>
+            O MongoDB é ótimo. Sem bancos de dados baseados em documentos, eu
+            ficaria preso no MySQL. O MongoDB é muito fácil recomendo bastante
+            para quem está iniciando na sua carreira como desenvolvedor(a).
+          </p>
+        </Item>
+        <Item color="#00718B">
+          <h1>MySQL</h1>
+          <p>
+            As tabelas... são legais até, só que eu prefiro usar bancos de dados
+            NoSQL (depende do projeto) baseados em documentos. Não me
+            interpretem mal, os bancos de dados relacionais ainda são úteis, e
+            eu estou perfeitamente bem em trabalhar com eles.
+          </p>
+        </Item>
+        <Item color="#F7C52D">
+          <h1>Firebase</h1>
+          <p>
+            {' '}
+            O google pensou muito bem em criar um banco de dados NoSQL desse
+            tipo, gosto bastante de implementar Firebase nos meus projetos
+            principalmente no Front-end que ajuda bastante.
+          </p>
+        </Item>
+        <Item color="#E94E31">
+          <h1>Git</h1>
+          <p>
+            Que tipo de desenvolvedor não usa o Git? É essencial para
+            praticamente qualquer projeto. O Git é ótimo no que faz, pois é um
+            sistema muito maduro. Ooh! Quase esqueci de{' '}
+            <ExternalLink href="https://github.com/zevdvlpr" color="#E94E31">
+              Meu github
+            </ExternalLink>
+            ! Confira :)
+          </p>
+        </Item>
+      </List>
+    </Container>
+  );
 };
 
 export default Skills;
--- src/components/Skills/styles.ts
+++ src/components/Skills/styles.ts
@@ -1,7 +1,117 @@
+import { shade } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100vh;
+  padding: 40px 80px;
 
-  background: orange;
+  background-color: ${({ theme }) => shade(0.3, theme.secondary)};
+
+  @media (max-width: 768px) {
+    padding: 40px 50px;
+  }
+`;
+
+export const Category = styled.h4`
+  font-size: 1.2rem;
+  font-weight: 500;
+
+  color: ${({ theme }) => theme.primary};
+
+  @media (max-width: 768px) {
+    font-size: 0.9rem;
+  }
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+  list-style: none;
+`;
+
+interface ItemProps {
+  color?: string;
+}
+
+export const Item = styled.li<ItemProps>`
+  width: 450px;
+  height: 220px;
+
+  margin: 30px;
+  padding: 30px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  border-radius: 6px;
+
+  background-color: ${({ color }) => color};
+
+  > h1 {
+    font-size: 1.8rem;
+  }
+
+  > p {
+    font-size: 0.9rem;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+
+    margin: 15px;
+    margin: 25px 30px;
+
+    h1 {
+      font-size: 1.5rem;
+    }
+
+    p {
+      font-size: 0.7rem;
+    }
+  }
+
+  @media (max-width: 320px) {
+    margin: 7px;
+  }
+`;
+
+interface LinkProps {
+  color: string;
+}
+
+export const ExternalLink = styled.a.attrs({
+  target: '_blank',
+  rel: 'noopener noreferrer',
+})<LinkProps>`
+  font-weight: 700;
+
+  color: ${({ theme }) => theme.white};
+
+  position: relative;
+
+  &::after {
+    height: 0.2rem;
+    width: 0;
+
+    margin-top: -0.2rem !important;
+    display: block;
+
+    position: absolute;
+    left: 0;
+
+    content: '';
+
+    background: ${({ color }) => shade(0.1, color)};
+    transition: width 0.3s ease, left 0.3s ease;
+  }
+
+  &:hover::after {
+    width: 100%;
+
+    left: 0;
+  }
 `;
