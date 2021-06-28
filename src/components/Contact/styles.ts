24bb26bdd726b438c80f2e6ebe1b24eb6bccf109
zevdvlpr <zevdvlpr@gmail.com>
2021-06-28 19:40:31 -0300

    ⭕ git: remove

--- src/components/Contact/styles.ts
+++
@@ -1,8 +0,0 @@
-import { shade } from 'polished';
-import styled from 'styled-components';
-
-export const Container = styled.section`
-  height: 100vh;
-
-  background-color: ${({ theme }) => shade(0.4, theme.secondary)};
-`;
--- src/styles/colors.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  primary: '#0080FF',
-  secondary: '#0E111F',
-  tertiary: '#00C6FE',
-
-  white: '#FFFFFF',
-  black: '#000000',
-};
