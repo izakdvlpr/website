a5bcdbb6cc580b04842b9b8b30d4a8c63a2a5efa
zevdvlpr <zevdvlpr@gmail.com>
2021-08-17 16:43:42 -0300

    ✨ feat: _document page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,59 @@
+import NextDocument, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+type InitialProps = {
+  styles: JSX.Element;
+  html: string;
+  head?: (JSX.Element | null)[] | undefined;
+};
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext): Promise<InitialProps> {
+    const sheet = new ServerStyleSheet();
+
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await NextDocument.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
+
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
