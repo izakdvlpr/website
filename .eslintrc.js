c915d7b4808f94b77e9c0ff074365344f0827c2c
zevdvlpr <zevdvlpr@gmail.com>
2021-08-17 16:39:30 -0300

    🎨 style: use @hitechline eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    '@hitechline/eslint-config/web',
+    '@hitechline/eslint-config/typescript',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/require-default-props': 'off',
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.tsx'],
-      },
-    ],
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'import/prefer-default-export': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    },
-  },
 };
