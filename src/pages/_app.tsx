a6ae89ed4a27d722d1399c5b737c5a3b3b75d288
zevdvlpr <zevdvlpr@gmail.com>
2021-12-31 17:18:06 -0300

    chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default App;
