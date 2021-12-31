5f67d669caa5f5eb0ce8de7e70e731a376114bc2
zevdvlpr <zevdvlpr@gmail.com>
2021-12-31 17:52:18 -0300

    react types

---
+++ src/interfaces/react.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
+import type { ReactNode } from 'react';
+
+export type TPropsWithChildren<T extends Record<string, any> = {}> = T & {
+  children: ReactNode;
+};
