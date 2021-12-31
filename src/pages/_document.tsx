8d068dc962cc52e1b1de85d18e7ca2adc8c9e450
zevdvlpr <zevdvlpr@gmail.com>
2021-12-31 17:58:01 -0300

    add poppins font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,19 @@ class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html lang="pt_BR">
-        <Head />
+        <Head>
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="*"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
 
         <body>
           <Main />
