2e6f0f94743fecc1f067a98618a19fdafb70cc39
zevdvlpr <zevdvlpr@gmail.com>
2021-12-31 17:15:33 -0300

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,25 @@ module.exports = {
     '@hitechline/eslint-config/web',
     '@hitechline/eslint-config/typescript',
   ],
+  rules: {
+    'react/require-default-props': 'off',
+
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        'alphabetize': {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'groups': [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'object',
+        ],
+      },
+    ],
+  },
 };
