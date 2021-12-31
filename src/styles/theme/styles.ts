5067d8b07e8041c5c0253e369a5cdc7ae9faed94
zevdvlpr <zevdvlpr@gmail.com>
2021-12-31 17:33:47 -0300

    chakra custom styles

---
+++ src/styles/theme/styles.ts
@@ -0,0 +1,12 @@
+export const styles: Record<string, any> = {
+  global: {
+    'a': {
+      _hover: {
+        textDecoration: 'none',
+      },
+    },
+    '*:focus': {
+      boxShadow: 'none !important',
+    },
+  },
+};
