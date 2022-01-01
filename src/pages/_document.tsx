9344a172bc391c43c98fd898a604b28375c8ff49
zevdvlpr <zevdvlpr@gmail.com>
2021-12-31 22:23:36 -0300

    remove fonts google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,19 +4,7 @@ class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html lang="pt_BR">
-        <Head>
-          {/* Google Fonts */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="*"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head />
 
         <body>
           <Main />
