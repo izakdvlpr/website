d07ea966e587dad71d3280ba19d8679b725c2317
zevdvlpr <zevdvlpr@gmail.com>
2021-12-31 22:23:25 -0300

    import fire-code font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
+import '@fontsource/fira-code/300.css';
+import '@fontsource/fira-code/400.css';
+import '@fontsource/fira-code/500.css';
+import '@fontsource/fira-code/700.css';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
+import { chakraTheme } from '@styles/theme';
+
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={chakraTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
