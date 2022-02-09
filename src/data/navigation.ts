9e94272bd0f1b42c427b02e4e7f568ae47665bf8
zevdvlpr <zevdvlpr@gmail.com>
2022-02-09 00:09:34 -0300

    navigation data

---
+++ src/data/navigation.ts
@@ -0,0 +1,19 @@
+export interface INavigation {
+  name: string;
+  href: string;
+}
+
+export const navigation: INavigation[] = [
+  {
+    name: 'About',
+    href: '/',
+  },
+  {
+    name: 'Blog',
+    href: '/',
+  },
+  {
+    name: 'Contact',
+    href: '/',
+  },
+];
