cc9bc264434bf70afc2c6c5fec294b55ab7a516d
zevdvlpr <zevdvlpr@gmail.com>
2022-02-09 00:09:55 -0300

    import poppins font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,18 @@ import '@fontsource/fira-code/300.css';
 import '@fontsource/fira-code/400.css';
 import '@fontsource/fira-code/500.css';
 import '@fontsource/fira-code/700.css';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 import { chakraTheme } from '@styles/theme';
 
-function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider theme={chakraTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
 }
-
-export default App;
