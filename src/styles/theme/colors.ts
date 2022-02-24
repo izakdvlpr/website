5ec1ecd4ae630e3dc0253761eba2efbcc5c9da25
zevdvlpr <zevdvlpr@gmail.com>
2022-02-24 19:13:32 -0300

    semantic tokens

--- src/styles/theme/colors.ts
+++
@@ -1,3 +0,0 @@
-import { Colors } from '@chakra-ui/react';
-
-export const colors: Colors = {};
--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,17 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
 
-import { colors } from './colors';
 import { components } from './components';
 import { config } from './config';
 import { fonts } from './fonts';
+import { semanticTokens } from './semanticTokens';
 import { styles } from './styles';
 
-const overrides = {
+export const theme = extendTheme({
   config,
-  colors,
+  semanticTokens,
   fonts,
   styles,
   components,
-};
-
-export const theme = extendTheme(overrides);
+});
