6c6f06a6dd2d632f2d7460c8459e2d55eb48cd63
zevdvlpr <zevdvlpr@gmail.com>
2022-02-24 19:12:54 -0300

    save color mode cookie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,23 @@ import '@fontsource/fira-code/700.css';
 import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/400.css';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  cookieStorageManager,
+  localStorageManager,
+} from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 import { theme } from '@styles/theme';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const colorModeManager =
+    typeof pageProps.cookies === 'string'
+      ? cookieStorageManager(pageProps.cookies)
+      : localStorageManager;
+
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme} colorModeManager={colorModeManager}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
